d21448646133016532d9125d1afd733e1b2ff884
Jake <jake.ord345@gmail.com>
2022-12-03 11:12:15 +0000

    day 3, day 2

--- 1/main.ts
+++ 1/main.ts
@@ -1,31 +1,27 @@
 import fs from "fs";
 import path from "path";
 
-(async () => {
-  const input = fs
-    .readFileSync(path.join(__dirname, "./input.txt"), "utf-8")
-    .split("\n");
+const input = fs.readFileSync(path.resolve("1/input.txt"), "utf-8").split("\n");
 
-  let group: string[] = [];
-  let totals: number[] = [];
+let group: string[] = [];
+let totals: number[] = [];
 
-  input.forEach(calorie => {
-    // Checks for empty string created by splitting on a new line
-    if (calorie === "") {
-      const groupTotal = group.reduce((a, b) => a + parseInt(b), 0);
-      totals.push(groupTotal);
-      // Reset group array to make room for the next one
-      group = [];
-    }
-    if (calorie !== "") group.push(calorie);
-  });
+input.forEach(calorie => {
+  // Checks for empty string created by splitting on a new line
+  if (calorie === "") {
+    const groupTotal = group.reduce((a, b) => a + parseInt(b), 0);
+    totals.push(groupTotal);
+    // Reset group array to make room for the next one
+    group = [];
+  }
+  if (calorie !== "") group.push(calorie);
+});
 
-  console.log("Highest Total Calories", Math.max(...totals));
+console.log("Highest Total Calories", Math.max(...totals));
 
-  const combinedTotals = totals
-    .sort((a, b) => b - a)
-    .slice(0, 3)
-    .reduce((a, b) => a + b, 0);
+const combinedTotals = totals
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((a, b) => a + b, 0);
 
-  console.log("Combined Total", combinedTotals);
-})();
+console.log("Combined Total", combinedTotals);
--- 2/main.ts
+++ 2/main.ts
@@ -1,4 +1,64 @@
 import fs from "fs";
 import path from "path";
 
-const input = fs.readFileSync(path.join(__dirname, "./input.txt"), "utf-8");
+const input = fs.readFileSync(path.resolve("2/input.txt"), "utf-8").split("\n");
+
+const scores = { win: 6, draw: 3, lost: 0, rock: 1, paper: 2, scissors: 3 };
+const opponent = { rock: "A", paper: "B", scissors: "C" };
+const me = { rock: "X", paper: "Y", scissors: "Z" };
+
+let score = 0;
+let strategyScore = 0;
+
+input.forEach(val => {
+  const opponentChoice = val.split(" ")[0];
+  const myChoice = val.split(" ")[1];
+
+  if (opponentChoice === opponent["rock"]) {
+    if (myChoice === me["rock"]) {
+      strategyScore += scores["scissors"] + scores["lost"];
+      score += scores["rock"] + scores["lost"];
+    }
+    if (myChoice === me["paper"]) {
+      strategyScore += scores["rock"] + scores["draw"];
+      score += scores["paper"] + scores["win"];
+    }
+    if (myChoice === me["scissors"]) {
+      strategyScore += scores["paper"] + scores["win"];
+      score += scores["scissors"] + scores["lost"];
+    }
+  }
+
+  if (opponentChoice === opponent["paper"]) {
+    if (myChoice === me["rock"]) {
+      strategyScore += scores["rock"] + scores["lost"];
+      score += scores["rock"] + scores["lost"];
+    }
+    if (myChoice === me["paper"]) {
+      strategyScore += scores["paper"] + scores["draw"];
+      score += scores["paper"] + scores["draw"];
+    }
+    if (myChoice === me["scissors"]) {
+      strategyScore += scores["scissors"] + scores["win"];
+      score += scores["scissors"] + scores["win"];
+    }
+  }
+
+  if (opponentChoice === opponent["scissors"]) {
+    if (myChoice === me["rock"]) {
+      strategyScore += scores["paper"] + scores["lost"];
+      score += scores["rock"] + scores["win"];
+    }
+    if (myChoice === me["paper"]) {
+      strategyScore += scores["scissors"] + scores["draw"];
+      score += scores["paper"] + scores["lost"];
+    }
+    if (myChoice === me["scissors"]) {
+      strategyScore += scores["rock"] + scores["win"];
+      score += scores["scissors"] + scores["draw"];
+    }
+  }
+});
+
+console.log("My Total Score", score);
+console.log("Strategy Score", strategyScore);

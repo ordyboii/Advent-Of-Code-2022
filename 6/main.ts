23d67e9362ce5bbfa60c6e8f4d3491ef37bfb537
Jake <jake.ord345@gmail.com>
2022-12-06 20:13:51 +0000

    6 was simple just overthought it

--- 6/main.ts
+++ 6/main.ts
@@ -2,3 +2,19 @@ import fs from "fs";
 import path from "path";
 
 const input = fs.readFileSync(path.resolve("6/input.txt"), "utf-8");
+
+for (let i = 0; i < input.length; i++) {
+  const characters = new Set(input.substring(i, i + 4).split(""));
+  if (characters.size === 4) {
+    console.log("The first package position", i + 4);
+    break;
+  }
+}
+
+for (let i = 0; i < input.length; i++) {
+  const characters = new Set(input.substring(i, i + 14).split(""));
+  if (characters.size === 14) {
+    console.log("The first message position", i + 14);
+    break;
+  }
+}
